3dbfa9efb626585a90aa070120ea8e01c457f719
bchelkow <blazej.chelkowski@outlook.com>
2016-07-12 12:08:31 +0200

    react injected not to body but to the div#main in body

--- app/views/layout/main.jsx
+++ app/views/layout/main.jsx
@@ -5,13 +5,11 @@ import Tree from '../tree/tree';
 export default class Main extends React.Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-3">
-            <Tree />
-          </div>
-          <div className="col-xs-9"></div>
+      <div className="row">
+        <div className="col-xs-3">
+          <Tree />
         </div>
+        <div className="col-xs-9"></div>
       </div>
     );
   }
